577330e54f1bdfcea3bb423d22e6573b2e387d0e
fjcal <javirulo93@gmail.com>
2016-10-22 13:41:11 +0200

    Se listan dispositivos pareados, algo falla con la conexion, es la llamada al método, no el método en si porque al comentarlo todo sigue fallando.

--- app/src/main/java/com/example/diego/gps/MainActivity.java
+++ app/src/main/java/com/example/diego/gps/MainActivity.java
@@ -4,13 +4,45 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Button;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
+import android.os.Handler;
+
+import java.util.ArrayList;
+import java.util.Set;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.UUID;
 
 public class MainActivity extends ActionBarActivity {
 
     private BluetoothAdapter BTAdapter;
+    private ArrayList<BluetoothDevice>pairedDevices;
+    ListView lv;
+
+
+    BluetoothSocket mmSocket = null;
+    BluetoothDevice mmDevice = null;
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+    byte[] readBuffer;
+    int readBufferPosition;
+    int counter;
+    volatile boolean stopWorker;
+    TextView myLabel;
 
     public static int REQUEST_BLUETOOTH = 1;
 
@@ -29,6 +61,43 @@ public class MainActivity extends ActionBarActivity {
             Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBT, REQUEST_BLUETOOTH);
         }
+
+        lv = (ListView)findViewById(R.id.listview);
+        myLabel = (TextView)findViewById(R.id.TBDatos);
+        Button openButton = (Button)findViewById(R.id.BOpen);
+        Button closeButton = (Button)findViewById(R.id.BClose);
+
+        openButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                    openBT();
+            }
+        });
+        closeButton.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                    closeBT();
+            }
+        });
+        list();
+
+        return;
+    }
+
+    public void list(){
+        Set<BluetoothDevice> lista = BTAdapter.getBondedDevices();
+        pairedDevices = new ArrayList<BluetoothDevice>();
+        ArrayList list = new ArrayList();
+
+        for(BluetoothDevice bt : lista) {
+            list.add(bt.getName());
+            pairedDevices.add(bt);
+        }
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        lv.setAdapter(adapter);
     }
 
     @Override
@@ -51,5 +120,95 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void openBT()
+    {
+        try {
+            mmDevice = pairedDevices.get(lv.getSelectedItemPosition());
+
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //Standard SerialPortService ID
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            mmSocket.connect();
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+
+            beginListenForData();
+
+            myLabel.setText("Bluetooth Opened");
+        }catch (IOException e){
+            Toast.makeText(getApplicationContext(),"Algo falla",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void closeBT()
+    {
+        try{
+            stopWorker = true;
+            mmOutputStream.close();
+            mmInputStream.close();
+            mmSocket.close();
+            myLabel.setText("Bluetooth Closed");
+        }catch (IOException e){
+            Toast.makeText(getApplicationContext(),"Algo falla",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        final byte delimiter = 10; //This is the ASCII code for a newline character
+
+        stopWorker = false;
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+        workerThread = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopWorker)
+                {
+                    try
+                    {
+                        int bytesAvailable = mmInputStream.available();
+                        if(bytesAvailable > 0)
+                        {
+                            byte[] packetBytes = new byte[bytesAvailable];
+                            mmInputStream.read(packetBytes);
+                            for(int i=0;i<bytesAvailable;i++)
+                            {
+                                byte b = packetBytes[i];
+                                if(b == delimiter)
+                                {
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    final String data = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+
+                                    handler.post(new Runnable()
+                                    {
+                                        public void run()
+                                        {
+                                            myLabel.setText(data);
+                                        }
+                                    });
+                                }
+                                else
+                                {
+                                    readBuffer[readBufferPosition++] = b;
+                                }
+                            }
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopWorker = true;
+                    }
+                }
+            }
+        });
+
+        workerThread.start();
     }
 }
